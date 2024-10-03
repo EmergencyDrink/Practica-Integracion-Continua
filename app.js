771d41fd11ac96a79e0264648e67b9f5a0d6c3bb
EmergencyDrink <andy123cx@gmail.com>
2024-10-03 01:59:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-<<<<<<< Updated upstream
-alert("Hola mundo")
-=======
+
 function HelloWorld() {
     return "Hola Mundo o Hello world";
 }
 
 module.exports = holaMundo;
->>>>>>> Stashed changes
+
